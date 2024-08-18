86b879deb7fe243bfd0b9ded9b99e374102c99c8
Daria <vip.fedorovadv@mail.ru>
2024-08-18 18:21:12 +0300

    2.1.2.Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String value;
+    public String getMessage() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
